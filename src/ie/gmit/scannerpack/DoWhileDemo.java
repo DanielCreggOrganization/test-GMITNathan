34ba24dd8ca4d9559b2e34def0d867733c3c2d14
GMITNathan <g00375584@gmit.ie>
2020-01-30 14:42:31 +0000

    lab

---
+++ src/ie/gmit/scannerpack/DoWhileDemo.java
@@ -0,0 +1,12 @@
+package ie.gmit.scannerpack;
+
+class DoWhileDemo {
+    public static void main(String[] args) {
+        int count = 1;
+        do {
+            System.out.println("Count is "+count);
+            count++;
+        }
+        while (count < 0);
+    }
+}
